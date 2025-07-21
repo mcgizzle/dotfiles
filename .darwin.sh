744977f5cc99ef5e1685cdd6e7e267585eac9a48
mcgizzle <sean@permutive.com>
2025-07-21 11:13:27 +0100

    feat: remove jenv

--- .darwin.sh
+++ .darwin.sh
@@ -2,9 +2,6 @@ fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
 
 source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
 source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
-# >>> scala-cli completions >>>
-fpath=("/Users/mcgizzle/Library/Application Support/ScalaCli/completions/zsh" $fpath)
-# <<< scala-cli completions <<<
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -12,7 +9,4 @@ export NVM_DIR="$HOME/.nvm"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-eval "$(rbenv init - --path)"
 [[ $commands[kubectl] ]] && source <(kubectl completion zsh)
-
-eval "$(jenv init -)"
